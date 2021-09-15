aaf1c79eae122e308c060eb1503658f6dbc74601
Dmitry <dishapby@gmail.com>
2021-09-16 02:34:11 +0300

    fix

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,9 +1,14 @@
 export default function createDreamTeam(arrayOfNames){
-    let result = '';
+    let result = [];
     arrayOfNames.forEach(item => {
         if(typeof item === "string"){
-            result += item[0]
+            result.push(item[0])
         }
     });
+
+    result.sort((a,b) => {
+        return a > b ? 1 : -1;
+    })
+
     return result;
 }
