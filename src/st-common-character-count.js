375d83ec80b2ec751cf625b8118b953d7ccb5b1d
Dmitry <dishapby@gmail.com>
2021-10-01 12:00:45 +0300

    getommonharacterount

---
+++ src/st-common-character-count.js
@@ -0,0 +1,10 @@
+export default function getCommonCharacterCount(a, b) {
+    let result = 0;
+
+    for(let i = 0; i < a.length; i++){
+        if(b.indexOf(a[i])){
+            result++;
+        }
+    }
+    console.log(result)
+}
