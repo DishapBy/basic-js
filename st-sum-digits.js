f73b4cfa52d3f26002211dd5a9e12cf890ef3ba8
Dmitry <dishapby@gmail.com>
2021-10-01 13:14:17 +0300

    getumfigits

---
+++ st-sum-digits.js
@@ -0,0 +1,19 @@
+export default function getSumOfDigits(numbers) {
+    let a = numbers.toString();
+    let result = 0;
+    a = [...a]
+
+    result = a.reduce((sum, current) => {
+        return Number(sum) + Number(current)
+    }, 0)
+
+    while (result >= 10){
+        let tmp = result.toString();
+        tmp = [...tmp];
+
+        result = tmp.reduce((sum, current) => {
+            return Number(sum) + Number(current)
+        }, 0)
+
+    }
+}
